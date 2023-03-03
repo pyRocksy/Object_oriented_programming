20a375b8bbcc8025a41f86760933afa09af3f95a
Biniam Feleke <100000687+pyRocksy@users.noreply.github.com>
2023-03-03 06:22:26 -0500

    Create test_calculator.py

---
+++ tests/test_calculator.py
@@ -0,0 +1,32 @@
+import unittest
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.c = Calculator()
+
+    # test for addition
+    def test_addition(self):
+        self.assertEqual(self.c.add(2, 3), 5)
+        self.assertEqual(self.c.add(0, 0), 0)
+        self.assertEqual(self.c.add(-2, -3), -5)
+
+    # test for subtraction
+    def test_subtraction(self):
+        self.assertEqual(self.c.subtract(5, 2), 3)
+        self.assertEqual(self.c.subtract(2, 5), -3)
+        self.assertEqual(self.c.subtract(0, 0), 0)
+
+    #  test for multiplication
+    def test_multiplication(self):
+        self.assertEqual(self.c.multiply(4, 6), 24)
+        self.assertEqual(self.c.multiply(0, 10), 0)
+        self.assertEqual(self.c.multiply(-2, 3), -6)
+
+    # test for multiplication
+    def test_division(self):
+        self.assertEqual(self.c.divide(8, 4), 2.0)
+        self.assertRaises(ValueError, self.c.divide, 8, 0)
+        self.assertAlmostEqual(self.c.divide(3, 2), 1.5, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
