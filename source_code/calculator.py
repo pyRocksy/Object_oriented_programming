b785e7c928cc3aa3ae5f73b24ba2d57bcb28d798
Biniam Feleke <100000687+pyRocksy@users.noreply.github.com>
2023-03-03 06:31:30 -0500

    Update calculator.py

--- source_code/calculator.py
+++ source_code/calculator.py
@@ -25,3 +25,4 @@ print(c.subtract(5, 2)) # Output: 3
 print(c.multiply(4, 6)) # Output: 24
 print(c.divide(8, 4))   # Output: 2.0
 print(c.divide(8, 0))   # Raises a ValueError with message "Cannot divide by zero!"
+
